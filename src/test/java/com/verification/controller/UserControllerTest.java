e55835dae22675572819a0fdbd48ca91593f3809
Zeeshan <zeeshanjamal_786@yahoo.com>
2018-07-21 15:49:17 +0500

    initial commit

--- src/test/java/com/verification/controller/UserControllerTest.java
+++ src/test/java/com/verification/controller/UserControllerTest.java
@@ -7,7 +7,6 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
